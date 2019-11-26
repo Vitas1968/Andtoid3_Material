d27c3f0673171ab867b1ec996606f6fdfebfcbe1
Vitas1968 <dor1968@mail.ru>
2019-11-26 19:55:43 +0700

    Принимает значение и выводит в текст вью

--- app/src/main/java/com/geek/andtoid3_material/fragments/FragmentPhoto2.java
+++ app/src/main/java/com/geek/andtoid3_material/fragments/FragmentPhoto2.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.geek.andtoid3_material.R;
 
@@ -17,15 +18,17 @@ import com.geek.andtoid3_material.R;
 public class FragmentPhoto2 extends Fragment {
 
 
-    public FragmentPhoto2() {
-        // Required empty public constructor
-    }
+    private TextView showName;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_photo2, container, false);
+        View view= inflater.inflate(R.layout.fragment_fragment_photo2, container, false);
+        showName=view.findViewById(R.id.show_name);
+        showName.setText(getArguments().getString("myName"));
+
+        return view;
     }
 
 }
