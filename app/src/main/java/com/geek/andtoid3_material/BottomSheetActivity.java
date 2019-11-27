e45c5ec6559d4b494d4a5a3669f918ad58b8a8dd
Vitas1968 <dor1968@mail.ru>
2019-11-27 21:07:52 +0700

    завел для экспериментов

---
+++ app/src/main/java/com/geek/andtoid3_material/BottomSheetActivity.java
@@ -0,0 +1,14 @@
+package com.geek.andtoid3_material;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BottomSheetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_sheet);
+    }
+}
