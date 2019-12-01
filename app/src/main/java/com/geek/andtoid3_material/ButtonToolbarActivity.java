e319e96b19c1df8b90917dbd35642a721e6c46f9
Vitas1968 <dor1968@mail.ru>
2019-12-01 10:45:31 +0700

    класс реалмзации ДЗ 3 урока

--- app/src/main/java/com/geek/andtoid3_material/ButtonToolbarActivity.java
+++ app/src/main/java/com/geek/andtoid3_material/ButtonToolbarActivity.java
@@ -1,18 +1,41 @@
 package com.geek.andtoid3_material;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.TextView;
 
-public class BottomSheetActivity extends AppCompatActivity {
-    private TextView showName;
+import java.util.Objects;
+
+public class ButtonToolbarActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bottom_sheet);
-        showName=findViewById(R.id.show_name);
-        showName.setText(getIntent().getStringExtra("myName"));
+        setContentView(R.layout.activity_button_toolbar);
+        mToolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        switch (id){
+            case android.R.id.home:
+                Log.d("TAG","onOptionsItemSelected: ");
+                onBackPressed();
+                default:
+                    return super.onOptionsItemSelected(item);
+
+        }
+
+
+    }
+
+
 }
