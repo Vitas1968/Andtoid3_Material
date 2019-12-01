f8ff9eac474895dda5d21c51c91ce94f14d5b884
Vitas1968 <dor1968@mail.ru>
2019-12-01 18:58:40 +0700

    класс обработчик экрана с тулбаром и плавающей кнопкой

--- a3l3designmodule/src/main/java/com/geek/a3l3designmodule/fragments/ToolBarFragment.java
+++ a3l3designmodule/src/main/java/com/geek/a3l3designmodule/fragments/ToolBarFragment.java
@@ -3,29 +3,53 @@ package com.geek.a3l3designmodule.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.geek.a3l3designmodule.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
+import java.util.Objects;
+
+
 public class ToolBarFragment extends Fragment {
+    private Toolbar mToolbar;
+    private TextView loginTextView;
 
-
-    public ToolBarFragment() {
-        // Required empty public constructor
-    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tool_bar, container, false);
+        View view=inflater.inflate(R.layout.fragment_tool_bar, container, false);
+        mToolbar=view.findViewById(R.id.toolbar);
+        loginTextView=view.findViewById(R.id.output_login_text_view);
+        AppCompatActivity activity=(AppCompatActivity)getActivity();
+        Objects.requireNonNull(activity).setSupportActionBar(mToolbar);
+        Objects.requireNonNull(activity.getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        String login=getArguments().getString("myLogin");
+        loginTextView.setText(login);
+
+        return view;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case android.R.id.home:
+                Log.d("TAGFR", "onOptionsItemSelected Fragment: ");
+                Objects.requireNonNull(getActivity()).onBackPressed();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
