31f362cfa31bef416ca64d602997426c97e115cc
Vitas1968 <dor1968@mail.ru>
2019-12-03 17:31:39 +0700

    реализация показа и сокрытия прогрессбара

--- a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/ui/ProgressBarFragment.java
+++ a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/ui/ProgressBarFragment.java
@@ -7,7 +7,10 @@ import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 import com.geek.a3l4drawerlayout.R;
 
@@ -15,17 +18,40 @@ import com.geek.a3l4drawerlayout.R;
  * A simple {@link Fragment} subclass.
  */
 public class ProgressBarFragment extends Fragment {
-
-
-    public ProgressBarFragment() {
-        // Required empty public constructor
-    }
+    private Button show,hide;
+    private ProgressBar mProgressBar;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_progress_bar, container, false);
+        View view=inflater.inflate(R.layout.fragment_progress_bar, container, false);
+        initViews(view);
+        setOnClickListenerShowButton();
+        setOnClickListenerHideButton();
+        return view;
+
+    }
+    private void initViews(View view){
+        show=view.findViewById(R.id.show);
+        hide=view.findViewById(R.id.hide);
+        mProgressBar=view.findViewById(R.id.progressBar);
+    }
+
+    private void setOnClickListenerShowButton(){
+        show.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mProgressBar.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+    private void setOnClickListenerHideButton(){
+        hide.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mProgressBar.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 
 }
