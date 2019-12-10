6226076e769c3eeea545c6603b6bf8f4882f418b
Vitas1968 <dor1968@mail.ru>
2019-12-10 21:16:38 +0700

    адаптер для рециклера

--- a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/recycler/RecyclerBoarAdapter.java
+++ a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/recycler/RecyclerBoarAdapter.java
@@ -1,6 +1,59 @@
 package com.geek.a3l4drawerlayout.recycler;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RecyclerBoarAdapter extends RecyclerView.Adapter {
+import com.geek.a3l4drawerlayout.R;
+
+public class RecyclerBoarAdapter extends RecyclerView.Adapter<RecyclerBoarAdapter.ViewHolder> {
+    private Fragment fragment;
+
+    public RecyclerBoarAdapter(Fragment fragment) {
+        this.fragment = fragment;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerBoarAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        View view = LayoutInflater.from(context)
+                .inflate(R.layout.card_view, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerBoarAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+    class ViewHolder extends RecyclerView.ViewHolder{
+        private  ImageView imageMember;
+        private TextView descriptMember;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageMember = itemView.findViewById(R.id.imgViewFamily);
+            descriptMember = itemView.findViewById(R.id.member_desript);
+        }
+
+        public void setImageMember(ImageView imageMember) {
+            this.imageMember = imageMember;
+        }
+
+        public void setDescriptMember(TextView descriptMember) {
+            this.descriptMember = descriptMember;
+        }
+    }
 }
