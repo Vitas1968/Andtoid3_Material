20e6c3e59120787fdac659e67cf10d405aa3ddb4
Vitas1968 <dor1968@mail.ru>
2019-12-11 21:40:47 +0700

    сделал список побольше

--- a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/data_model/DataModel.java
+++ a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/data_model/DataModel.java
@@ -7,35 +7,43 @@ import java.util.ArrayList;
 public class DataModel {
     private ArrayList<String> listMemberFamily;
     private ArrayList<Integer> listImgMemberFamily;
-    private  ArrayList<ArrayList> listResult;
+    private ArrayList<ArrayList> listResult;
 
     public DataModel() {
         this.listMemberFamily = new ArrayList<>();
         this.listImgMemberFamily = new ArrayList<>();
-        this.listResult=new ArrayList<>();
+        this.listResult = new ArrayList<>();
         initlistMemberFamily();
         initlistImgMemberFamily();
-        resultList();
     }
-    private ArrayList initlistMemberFamily(){
+
+    private ArrayList initlistMemberFamily() {
         listMemberFamily.add("Свинопапа");
         listMemberFamily.add("Свиномама");
         listMemberFamily.add("Свиносына");
+        listMemberFamily.add("Свинобрат");
+        listMemberFamily.add("Свиносестра");
+        listMemberFamily.add("Свинодоча");
         return listMemberFamily;
     }
-    private ArrayList initlistImgMemberFamily(){
+
+    private ArrayList initlistImgMemberFamily() {
+        listImgMemberFamily.add(R.drawable.boar);
+        listImgMemberFamily.add(R.drawable.pig);
+        listImgMemberFamily.add(R.drawable.pig_mini);
         listImgMemberFamily.add(R.drawable.boar);
         listImgMemberFamily.add(R.drawable.pig);
         listImgMemberFamily.add(R.drawable.pig_mini);
         return listImgMemberFamily;
     }
-    private    ArrayList resultList(){
+
+    private ArrayList resultList() {
         listResult.add(listMemberFamily);
-        listResult.add(listMemberFamily);
+        listResult.add(listImgMemberFamily);
         return listResult;
     }
 
-    public ArrayList<ArrayList> getListResult() {
-        return listResult;
+    public ArrayList getListResult() {
+        return resultList();
     }
 }
