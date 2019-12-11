476bdedbe7b2c1e149270f882393e2455c75fd2f
Vitas1968 <dor1968@mail.ru>
2019-12-11 21:46:59 +0700

    инициализация рециклера во фрагменте

--- a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/ui/gallery/GalleryFragment.java
+++ a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/ui/gallery/GalleryFragment.java
@@ -7,8 +7,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.geek.a3l4drawerlayout.R;
+import com.geek.a3l4drawerlayout.data_model.DataModel;
+import com.geek.a3l4drawerlayout.recycler.RecyclerBoarAdapter;
+import java.util.ArrayList;
 
 public class GalleryFragment extends Fragment {
 
@@ -16,7 +20,7 @@ public class GalleryFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-
+        initRecycler(root);
         return root;
     }
 
@@ -25,8 +29,16 @@ public class GalleryFragment extends Fragment {
         super.onAttach(context);
         mContext = context;
     }
-    private void initRecycler(){
 
+    private void initRecycler(View view) {
+        DataModel dataModel = new DataModel();
+        ArrayList listResult = dataModel.getListResult();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(mContext);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        RecyclerBoarAdapter adapter = new RecyclerBoarAdapter(this, listResult);
+        RecyclerView recycler = view.findViewById(R.id.recycler_view);
+        recycler.setLayoutManager(layoutManager);
+        recycler.setAdapter(adapter);
     }
 
 }
