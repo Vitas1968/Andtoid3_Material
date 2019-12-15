88e90134612743e3288f1f95c6aa355aac7fb82e
Vitas1968 <dor1968@mail.ru>
2019-12-15 19:18:59 +0700

    адаптер для вкладок

--- a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/tabs_fragments/TabsAdapter.java
+++ a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/tabs_fragments/TabsAdapter.java
@@ -1,6 +1,42 @@
 package com.geek.a3l4drawerlayout.tabs_fragments;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import java.util.ArrayList;
+
 public class TabsAdapter extends FragmentPagerAdapter {
+    ArrayList<Fragment> listFragments;
+    ArrayList<String> listTitleTabs;
+
+    public TabsAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+        listFragments=new ArrayList<>(3);
+        listTitleTabs=new ArrayList<>(3);
+    }
+
+    public  void addFragment(Fragment fragment, String title){
+        listFragments.add(fragment);
+        listTitleTabs.add(title);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return listFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return listFragments.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return listTitleTabs.get(position);
+    }
 }
