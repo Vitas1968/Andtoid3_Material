d5f6d778111e44239c54c94b2d124fccbf729545
Vitas1968 <dor1968@mail.ru>
2019-12-15 19:21:14 +0700

    вкладка с описанием члена семьи

--- a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/tabs_fragments/DescriptFragment.java
+++ a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/tabs_fragments/DescriptFragment.java
@@ -2,30 +2,39 @@ package com.geek.a3l4drawerlayout.tabs_fragments;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import com.geek.a3l4drawerlayout.R;
+
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DescriptFragment extends Fragment {
 
-
     public DescriptFragment() {
-        // Required empty public constructor
     }
 
+    public static DescriptFragment newInstance(Bundle bundle) {
+        DescriptFragment descriptFragment = new DescriptFragment();
+        descriptFragment.setArguments(bundle);
+        return descriptFragment;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_descript, container, false);
+        View view = inflater.inflate(R.layout.fragment_descript, container, false);
+        Bundle bundle = getArguments();
+        String description = Objects.requireNonNull(bundle).getString("descriptArgs");
+        TextView descriptView = view.findViewById(R.id.descript_textView);
+        descriptView.setText(description);
+        return view;
     }
 
 }
