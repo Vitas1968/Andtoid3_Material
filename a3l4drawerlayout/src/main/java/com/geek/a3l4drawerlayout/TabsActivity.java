b9884bee980fe0de06728d4719868770c53e7b20
Vitas1968 <dor1968@mail.ru>
2019-12-15 19:18:31 +0700

    Реализвция активити вкладок

--- a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/TabsActivity.java
+++ a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/TabsActivity.java
@@ -1,14 +1,48 @@
 package com.geek.a3l4drawerlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.geek.a3l4drawerlayout.tabs_fragments.AgeFragment;
+import com.geek.a3l4drawerlayout.tabs_fragments.DescriptFragment;
+import com.geek.a3l4drawerlayout.tabs_fragments.ImgFragment;
+import com.geek.a3l4drawerlayout.tabs_fragments.TabsAdapter;
+import com.google.android.material.tabs.TabLayout;
+
 public class TabsActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabs);
+        Bundle bundle=getIntent().getBundleExtra("args");
+/*
+        String tmpStr = bundle.getString("descriptArgs");
+        int tmpImg = bundle.getInt("imgArgs");
+        int tmpAge = bundle.getInt("ageArgs");
+ */
+
+        ImgFragment imgFrag=ImgFragment.newInstance(bundle);
+        DescriptFragment descrFrag=DescriptFragment.newInstance(bundle);
+        AgeFragment ageFrag=AgeFragment.newInstance(bundle);
+        ViewPager viewPager = findViewById(R.id.view_pager);
+
+        TabsAdapter tabsAdapter=new TabsAdapter(getSupportFragmentManager());
+        tabsAdapter.addFragment(imgFrag,"Photo");
+        tabsAdapter.addFragment(descrFrag,"Description");
+        tabsAdapter.addFragment(ageFrag,"Age");
+
+        viewPager.setAdapter(tabsAdapter);
+        TabLayout tabLayout=findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+
     }
+
+
 }
