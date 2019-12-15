029b9c6594ee1e2a93e70928bc28b9c2f51c4c79
Vitas1968 <dor1968@mail.ru>
2019-12-15 19:22:14 +0700

    владка с возратом члена семьи

--- a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/tabs_fragments/AgeFragment.java
+++ a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/tabs_fragments/AgeFragment.java
@@ -2,30 +2,31 @@ package com.geek.a3l4drawerlayout.tabs_fragments;
 
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 import com.geek.a3l4drawerlayout.R;
+import java.util.Objects;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class AgeFragment extends Fragment {
 
-
     public AgeFragment() {
-        // Required empty public constructor
     }
-
+    public static AgeFragment newInstance(Bundle bundle){
+        AgeFragment ageFragment = new AgeFragment();
+        ageFragment.setArguments(bundle);
+        return ageFragment;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_age, container, false);
+        View view =inflater.inflate(R.layout.fragment_age, container, false);
+        Bundle bundle= getArguments();
+        int age = Objects.requireNonNull(bundle).getInt("ageArgs");
+        TextView ageTextView=view.findViewById(R.id.age_textView);
+        ageTextView.setText(Integer.toString(age));
+        return view;
     }
-
 }
