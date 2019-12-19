37688771c02799e3690d3f04a68a0a46f3002a74
Vitas1968 <dor1968@mail.ru>
2019-12-19 21:00:04 +0700

    Добавлены активити и разметка для первых двух заданий

---
+++ a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/StyleActivity.java
@@ -0,0 +1,14 @@
+package com.geek.a3l4drawerlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StyleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_style);
+    }
+}
