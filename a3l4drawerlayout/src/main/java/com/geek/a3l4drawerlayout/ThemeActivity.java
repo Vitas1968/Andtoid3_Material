6a6878c404d65a8a8cd9fd1200e7bbf0d8ef44bc
Vitas1968 <dor1968@mail.ru>
2019-12-19 21:19:28 +0700

    флаг длительности показа снекбара теперь задается вот так

--- a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/ThemeActivity.java
+++ a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/ThemeActivity.java
@@ -3,6 +3,7 @@ package com.geek.a3l4drawerlayout;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,7 +24,7 @@ public class ThemeActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                Snackbar.make(view, "Replace with your own action", BaseTransientBottomBar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
     }
