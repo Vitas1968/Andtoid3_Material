398e1d34273920a77ccc7e789a8a42e3f937b67f
Vitas1968 <dor1968@mail.ru>
2019-12-19 21:06:51 +0700

    Я сделал третье задание несколько по другому через преференцес что бы не дублировать код.(Творчество же в жопе свербит))) Здесь я создаю и сохраняю выбранную тему.

--- a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/MainActivity.java
+++ a3l4drawerlayout/src/main/java/com/geek/a3l4drawerlayout/MainActivity.java
@@ -1,51 +1,75 @@
 package com.geek.a3l4drawerlayout;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
-
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.Menu;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    public static final String EXTRA_THEME = "EXTRA_THEME";
+    private int themeNumber;
+    private int currentThemeId=-1;
+    public static final String APP_PREFERENCES = "mysettings";
+    public static final String APP_PREFERENCES_CURRENT_THEME_ID = "currentThemeId";
+    private SharedPreferences settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+
+            themeNumber = savedInstanceState.getInt(EXTRA_THEME);
+
+            switch (themeNumber) {
+                case 0:
+                    setTheme(R.style.AppTheme);
+                    currentThemeId= R.style.AppTheme;
+                    break;
+                case 1:
+                    setTheme(R.style.AppThemePurple);
+                    currentThemeId= R.style.AppThemePurple;
+                    break;
+                case 2:
+                    setTheme(R.style.AppThemeBrown);
+                    currentThemeId= R.style.AppThemeBrown;
+                    break;
+            }
+        }
         setContentView(R.layout.activity_main);
+        settings=getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                Snackbar.make(view, "Replace with your own action", BaseTransientBottomBar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send,
-                R.id.progressBarFragment).setDrawerLayout(drawer).build();
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_tools, R.id.nav_share, R.id.nav_send, R.id.progressBarFragment).setDrawerLayout(drawer).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
@@ -59,8 +83,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+        Editor editor=settings.edit();
+        editor.putInt(APP_PREFERENCES_CURRENT_THEME_ID,currentThemeId);
+        editor.apply();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.standard:
+                themeNumber = 0;
+                break;
+            case R.id.purple:
+                themeNumber = 1;
+                break;
+            case R.id.brown:
+                themeNumber = 2;
+                break;
+
+        }
+        recreate();
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(EXTRA_THEME, themeNumber);
     }
 }
